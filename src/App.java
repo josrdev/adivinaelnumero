c18e3aa519692617ba4643e6a19e978c7c0144cb
Joseph Fuentes <josephfv2812@gmail.com>
2025-11-14 15:48:14 +0100

    Mejora de la estructura del programa

--- src/App.java
+++ src/App.java
@@ -2,26 +2,44 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(" █████╗ ██████╗ ██╗██╗   ██╗██╗███╗   ██╗ █████╗     ███████╗██╗         ███╗   ██╗██╗   ██╗███╗   ███╗███████╗██████╗  ██████╗ ");
-        System.out.println("██╔══██╗██╔══██╗██║██║   ██║██║████╗  ██║██╔══██╗    ██╔════╝██║         ████╗  ██║██║   ██║████╗ ████║██╔════╝██╔══██╗██╔═══██╗");
-        System.out.println("███████║██║  ██║██║██║   ██║██║██╔██╗ ██║███████║    █████╗  ██║         ██╔██╗ ██║██║   ██║██╔████╔██║█████╗  ██████╔╝██║   ██║");
-        System.out.println("██╔══██║██║  ██║██║╚██╗ ██╔╝██║██║╚██╗██║██╔══██║    ██╔══╝  ██║         ██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══╝  ██╔══██╗██║   ██║");
-        System.out.println("██║  ██║██████╔╝██║ ╚████╔╝ ██║██║ ╚████║██║  ██║    ███████╗███████╗    ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║███████╗██║  ██║╚██████╔╝");
-        System.out.println("╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝    ╚══════╝╚══════╝    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ");
+        System.out.println(
+                " █████╗ ██████╗ ██╗██╗   ██╗██╗███╗   ██╗ █████╗     ███████╗██╗         ███╗   ██╗██╗   ██╗███╗   ███╗███████╗██████╗  ██████╗ ");
+        System.out.println(
+                "██╔══██╗██╔══██╗██║██║   ██║██║████╗  ██║██╔══██╗    ██╔════╝██║         ████╗  ██║██║   ██║████╗ ████║██╔════╝██╔══██╗██╔═══██╗");
+        System.out.println(
+                "███████║██║  ██║██║██║   ██║██║██╔██╗ ██║███████║    █████╗  ██║         ██╔██╗ ██║██║   ██║██╔████╔██║█████╗  ██████╔╝██║   ██║");
+        System.out.println(
+                "██╔══██║██║  ██║██║╚██╗ ██╔╝██║██║╚██╗██║██╔══██║    ██╔══╝  ██║         ██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══╝  ██╔══██╗██║   ██║");
+        System.out.println(
+                "██║  ██║██████╔╝██║ ╚████╔╝ ██║██║ ╚████║██║  ██║    ███████╗███████╗    ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║███████╗██║  ██║╚██████╔╝");
+        System.out.println(
+                "╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝    ╚══════╝╚══════╝    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ");
         System.out.println("");
 
         int num = 0, numrandom;
 
         Scanner sc = new Scanner(System.in);
-        
-        numrandom = (int)(Math.random() * 10) + 1;
 
-        do{
-            System.out.println("Intente acertar el numemro!");
-            System.out.print("> ");
-            num = sc.nextInt();
-        } while (numrandom != num);
-            System.out.println("Has acertado el numero!");
-        System.out.println("Felicidades!!!");
+        /* Se crea un numero random */
+        numrandom = (int) (Math.random() * 10) + 1;
+
+        while (true) {
+            try {
+                do {
+                    System.out.println("\nIntente acertar el numemro!");
+                    System.out.print("> ");
+                    num = sc.nextInt();
+
+                    if (num < 0) {
+                        throw new IllegalArgumentException();
+                    }
+                } while (numrandom != num);
+                System.out.println("Has acertado el numero!");
+                System.out.println("Felicidades!!!");
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("El número no es valido!");
+            }
+        }
     }
 }
